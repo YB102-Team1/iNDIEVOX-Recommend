a3bbf88f3d6c796f467cce221ef70fbdf48eea5b
SamasLin <samas0120@gmail.com>
2015-03-12 18:17:47 +0800

    Revert "同步資料"
    This reverts commit 4490f0e4a369139d546d0a9c83b7186d773b6d10.

--- sync.php
+++ sync.php
@@ -7,7 +7,7 @@ echo "<pre>";
 foreach (glob(DATA_SQL_ROOT.'/*_structure.sql') as $sql_path) {
     $sql = file_get_contents($sql_path);
     $create_result = $db_obj->query($sql);
-    // var_dump($create_result);
+    var_dump($create_result);
     echo "Table ".str_replace('_structure.sql', '', str_replace(DATA_SQL_ROOT.'/', '', $sql_path))." created.\n";
 }
 
@@ -18,7 +18,7 @@ foreach ($tables as $table_name) {
     foreach (glob(DATA_SQL_ROOT.'/'.$table_name.'__*.sql') as $sql_path) {
         $sql = file_get_contents($sql_path);
         $create_result = $db_obj->query($sql);
-        // var_dump($create_result);
+        var_dump($create_result);
         echo "\tsegment ".str_replace('.sql', '', str_replace(DATA_SQL_ROOT.'/'.$table_name.'_', '', $sql_path))." imported.\n";
     }
     echo "\n";

444611f17065cf89474930fe37f64e7f64cb7bb0
SamasLin <samas0120@gmail.com>
2015-03-12 21:01:39 +0800

    優化同步資料腳本

--- _asset/python/split-sql.py
+++ _asset/python/split-sql.py
@@ -1,21 +1,21 @@
 # -*- coding:utf-8 -*-
 # __author__ = 'Samas Lin<samas0120@gmail.com>'
-import sys
+import sys, os
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
 tables = ['buy_disc_record', 'buy_song_record', 'disc', 'favorite', 'song']
 for table in tables:
-    source_file_name = '../sql/data/' + table + '.sql'
+    source_file_name = os.path.dirname(os.path.abspath(__file__)) + '/../sql/data/' + table + '.sql'
     source_file = open(source_file_name, 'r')
     contents = source_file.read().split('INSERT INTO');
     source_file.close();
     counter = 1
     for content in contents[1:]:
-        target_file_name = '../sql/data/' + table + '_' + str('%05d' %counter) + '.seg';
+        target_file_name = os.path.dirname(os.path.abspath(__file__)) + '/../sql/data/' + table + '_' + str('%05d' %counter) + '.seg';
         target_file = open(target_file_name, 'w')
         target_file.write('INSERT INTO' + content)
         target_file.close()
         counter = counter + 1
-    print table + ' => done'
+    print table + ' => ' + str(counter)
--- run/sync.php
+++ run/sync.php
@@ -1,8 +1,12 @@
 <?php
+echo "<pre>";
+$command = escapeshellcmd("python ".$_SERVER['DOCUMENT_ROOT']."/_asset/python/split-sql.py");
+$output = shell_exec($command);
+echo "<strong>[$command]:</strong>\n$output\n";
+
 include $_SERVER['DOCUMENT_ROOT'].'/_config/system_config.inc';
 $db_obj = new DatabaseAccess();
 $tables = array('buy_disc_record', 'buy_song_record', 'disc', 'favorite', 'song');
-echo "<pre>";
 
 foreach (glob(DATA_SQL_ROOT.'/*_structure.sql') as $sql_path) {
     $sql = file_get_contents($sql_path);
@@ -20,7 +24,7 @@ foreach ($tables as $table_name) {
         $create_result = $db_obj->query($sql);
         // var_dump($create_result);
     }
-    echo "\tsegment ".str_replace('.seg', '', str_replace(DATA_SQL_ROOT.'/'.$table_name.'_', '', $sql_path))." imported.\n\n";
+    echo "\t".str_replace('.seg', '', str_replace(DATA_SQL_ROOT.'/'.$table_name.'_', '', $sql_path))." segments imported.\n\n";
 }
 
 echo "</pre>";

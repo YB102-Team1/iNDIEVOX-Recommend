c2bd9ded685bb683cad283b7eb26511fc3c592c0
M.C. Shih <abc780922@gmail.com>
2015-03-24 13:47:40 +0800

    train_model and test_model

--- _asset/R/SOMO.R
+++ _asset/R/SOMO.R
@@ -3,49 +3,52 @@ conn <- odbcConnect("BigData", uid="team1" , pwd="yb102")
 data_table <- sqlTables(conn) 
 
 
-df <- sqlQuery(conn, "select user_id, on_thing_id, type, is_purchased, is_liked, genre from train_model")
+df <- sqlQuery(conn, "select id, user_id, on_thing_id, type, is_purchased, is_liked, genre from train_model")
 close(conn)
-names (df) <-c ("user","item","type", "buy","like","genre") 
-disc = df[df$type == "disc" , -3]
+names (df) <-c ("id","user","item","type", "buy","like","genre") 
+disc = df[df$type == "disc" , -4]
 
-# Read data set
+train_disc = disc[which(disc$id%%5 !=0),]
+train_disc = train_disc[,-1]
+test_disc = disc[which(disc$id%%5 ==0),]
+test_disc = test_disc[,-1]
+#Read data set
 #original <-read.csv (file = "train_model.csv", header = FALSE)
 #names (original) <-c ("id","user","item","type", "buy","like","genre") 
 
-#?z???Xdisc
-#disc = original[original$type == "disc" , c(2,3,5,6,7)]
+#篩選出disc
+#disc = original[original$type == "song" , c(2,3,5,6,7)]
 
-#??buy?Mfavorite???w????????
-disc = cbind(disc,5)
-disc[which(disc[,3]==1&disc[,4]==0),6]=3
-disc[which(disc[,3]==0&disc[,4]==1),6]=2
+#依buy和favorite給定分數
+train_disc = cbind(disc,5)
+train_disc[which(train_disc$buy==1&train_disc$like==0),6]=3
+train_disc[which(train_disc$buy==0&train_disc$like==1),6]=2
 
-colnames(disc) = c('user','item','buy','like','genre','score')
+train_disc = train_disc[, c(2,3,6,7)]
+colnames(train_disc) = c('user','item','genre','score')
 
-disc = disc[, c(1,2,5,6)]
 
+#取disc分類
+cluster = train_disc[train_disc$genre=="1", -3]
 
-#??disc???????@?s
-cluster_1 = disc[disc$genre=="1", -3]
-
-#???]?Yuser???b?[???Yitem
+#假設某user正在觀看某item
 user=1
 item=808
-if(length(which(cluster_1$user==user))==0){
-  cluster_1 = rbind(cluster_1,c(user,item))
-  cluster_1[-1,3]=5
+if(length(which(cluster$user==user))==0){
+  cluster = rbind(cluster,c(user,item))
+  cluster[-1,3]=5
 }
 
 
 # Calculated User Lists
 usersUnique <-function () {
-  users <-unique (cluster_1 $ user)
+  users <-unique (cluster $ user)
   users [order (users)]
 }
 
 # Calculation Method Product List
 itemsUnique <-function () {
-  items <-unique (cluster_1 $ item)
+  items <-unique (cluster $ item)
   items [order (items)]
 }
 
@@ -55,40 +58,40 @@ users <-usersUnique ()
 # Product List
 items <-itemsUnique () 
 
-#?N?????????????s
+#將使用者分群
 m=matrix (rep (0, length(users) * 2), nrow = length(users))
 
 for(i in 1:length(users)){ 
-  #???X??????users[i]??????
-  len = length(which(cluster_1$user==users[i]))
+  #算出使用者users[i]的個數
+  len = length(which(cluster$user==users[i]))
   
-  #???X??????users[i]???g?????L?????????~???M
-  sum = sum(cluster_1[which(cluster_1$user==users[i]),3])
+  #算出使用者users[i]曾經評價過的所有物品的和
+  sum = sum(cluster[which(cluster$user==users[i]),3])
   
-  #m?x???????????P????????
+  #m矩正為使用者與其平均分
   m[i,1] = users[i]
   m[i,2] = round(sum/len,digits=2) 
 }
 
 
-
+#矩陣相乘
 f = function(user){
   
-  #???X?M???J??user?P?s??user
+  #找出和輸入的user同群的user(四捨五入同分的user)
   user_avg_score = m[which(m[,1] == user),2]
   r = round(user_avg_score,digits=0)
   members = m[which(round(m[,2],digits=0) == r),]
   
   score_of_members = matrix(- members[,2],byrow=T,nrow=length(items),ncol=length(members[,1]))
   
-  #???X?P?suser?磌?~J??????
+  #找出同群user對物品J的評價
   for(i in 1:length(items)){
     item = items[i]
     for(j in 1:length(members[,1])){           
-      member = which(cluster_1$user==members[j,1])
+      member = which(cluster$user==members[j,1])
       for(k in member){
-        if(cluster_1[k,2]==item){
-          obj = cluster_1[k,3]         
+        if(cluster[k,2]==item){
+          obj = cluster[k,3]         
           score_of_members[i,j] = obj  + score_of_members[i,j]
         }
       }     
@@ -118,16 +121,12 @@ f = function(user){
   colnames(result)=c('item','pref')
   result = result[order(result[,2],decreasing=T),]
   return(result) 
-    
-  
-  
-  
   
 }
 
 
 
-test = f(311)
+test2 = f(1)
 
 
 
